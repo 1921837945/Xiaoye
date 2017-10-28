58feff5f46a4aa2a20ff638da1a6c5c7fc4d21c0
1921837945 <30437776+1921837945@users.noreply.github.com>
2017-10-29 00:08:44 +0800

    Update lyb.php

--- lyb.php
+++ lyb.php
@@ -10,6 +10,7 @@ if (!$con)
     die('Could not connect: ' . mysqli_error($con));
 }
 mysqli_select_db( $con,"mylyb");//选择数据库
+date_default_timezone_set("Asia/Shanghai");  //设置时区
 
 
 echo "<table border='1'>";
@@ -17,8 +18,8 @@ echo "<tr><th>留言者</th><th>留言内容</th><th>时间</th><th>操作</th><
 
 //$s1=$_POST[writer];
 //$s2=$_POST[content];
-$sql = " INSERT INTO liuyan(writer,content)
- VALUES('$_POST[writer]','$_POST[content]')";//向数据库表中插入新记录
+$sql = " INSERT INTO liuyan(writer,content,sj)
+ VALUES('$_POST[writer]','$_POST[content]',now())";//向数据库表中插入新记录
 
 if (!mysqli_query($con,$sql))
 {
@@ -32,7 +33,7 @@ while($row = mysqli_fetch_array($result))
     echo "<tr>";
     echo "<td aligh ='center'>$row[writer]</td>";
     echo "<td aligh ='center'>$row[content]</td>";
-    echo "<td aligh ='center'>$row[content]</td>";
+    echo "<td aligh ='center'>$row[sj]</td>";
     echo "<td aligh ='center'><a href=lyb_delete.php?writer=$row[writer]>删除</a></td>";
     echo "</tr>";
     echo "<br>";
