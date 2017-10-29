b26196a1089a19a04c464598ca5117de14ad8c40
1921837945 <30437776+1921837945@users.noreply.github.com>
2017-10-29 13:26:48 +0800

    Update lab_delete.php

--- lab_delete.php
+++ lab_delete.php
@@ -1,17 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <center><a href="menu.php">目录</a></center>
+</head>
+<body>
+    <center>
+        <?php
 
-$con = mysqli_connect("localhost","root","");
-mysqli_select_db( $con,"mylyb");
-$writer = $_GET['writer'];
-$sql = "delete from liuyan where writer='".$writer."'";
-mysqli_query($con,$sql);
-$mark  = mysqli_affected_rows($con);
+        $con = mysqli_connect("localhost","root","");
+        mysqli_select_db( $con,"mylyb");
+        $lou = $_GET['lou'];
+        $sql = "delete from liuyan where lou ='".$lou."'";
+        mysqli_query($con,$sql);
+        $mark  = mysqli_affected_rows($con);
 
-if($mark>0){
-    echo "成功";
-}else{
-    echo "失败";
-}
+        if($mark>0){
+            echo "留言删除成功";
+        }else{
+            echo "留言删除失败";
+        }
 
-mysqli_close($con);
-?>
+        mysqli_close($con);
+        ?>
+    </center>
+
+</body>
+
+</html>
