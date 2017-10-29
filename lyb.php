fa5b2a289ad8ab8239a105b7704f86ddc226fe91
1921837945 <30437776+1921837945@users.noreply.github.com>
2017-10-29 14:55:16 +0800

    Update lyb.php

--- lyb.php
+++ lyb.php
@@ -6,16 +6,16 @@
 <?php
     $con = mysqli_connect("localhost","root","");
     mysqli_select_db( $con,"mylyb");
-    date_default_timezone_set("Asia/Shanghai");  //设置时区
+    date_default_timezone_set("Asia/Shanghai");
 
     echo "<table border='1'>";
     echo "<tr><th>留言者</th><th>留言内容</th><th>时间</th><th>操作</th></tr>";
 
     $sql = " INSERT INTO liuyan(writer,content,sj)
-    VALUES('$_POST[writer]','$_POST[content]',now())";//向数据库表中插入新记录
+    VALUES('$_POST[writer]','$_POST[content]',now())";
 
     mysqli_query($con,$sql);
-    $result = mysqli_query($con,"SELECT * FROM liuyan");//查看liuyan表所有的数据
+    $result = mysqli_query($con,"SELECT * FROM liuyan");
     while($row = mysqli_fetch_array($result))
     {
         echo "<tr>";
@@ -25,10 +25,8 @@
         echo "<td aligh ='center'><a href=lyb_delete.php?writer=$row[writer]>删除</a></td>";
         echo "</tr>";
         echo "<br>";
-    }//循环输出数据
-
+    }
     echo "</table>";
-
 
     mysqli_close($con);
     ?>
